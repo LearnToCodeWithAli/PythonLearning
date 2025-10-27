2b675df6841a806f06229ce536c92d05187cc98d
LearnToCodeWithAli <learn.to.code.with.ali@gmail.com>
2025-10-26 21:50:11 -0700

    intro to python from 10/26

---
+++ intro to python/intro_to_python_10_23.py
@@ -0,0 +1,71 @@
+
+# data types, data structures,
+# loops, conditional statements
+
+title = "Pinky and the Brain" # string
+number_of_mice = 2_000_000_000_000 # int - whole number
+value_of_pi = 3.141_5926 # float
+
+multiline_string = """
+hi this is a multiline comment 
+"""
+
+# print(multiline_string)
+
+is_this_boring = True
+
+# try and except
+# classes, oop
+# calculator, csv
+
+# lists, tuples, sets
+
+#list - ordered, indexable, mutable, allows duplicates
+drinks = ["pina colada", "sprite", "apple juice"]
+
+# print(drinks[0])
+# drinks[0] = "strawberry lemonade"
+# print(drinks)
+
+# drinks.append("guava juice")
+# print(drinks)
+# drinks.remove("sprite")
+# print(drinks)
+# del drinks[1]
+# print(drinks)
+
+# tuple - ordered, indexable, not mutable, duplicates allowed
+# makeup_brands = ("Morphe", "Huda", "Milk", "Fenty", "Dior")
+# print(makeup_brands[3])
+
+# set - not ordered, not indexable, mutable, no duplicates
+makeup_tools = {"curler", "brush", "mirror", "sponge"}
+# print(makeup_tools)
+# makeup_tools.add("tweezers")
+# print(makeup_tools)
+# makeup_tools.remove("brush")
+# print(makeup_tools)
+
+if "brush" in makeup_tools:
+    print("Do not buy the brush")
+
+
+
+# dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
